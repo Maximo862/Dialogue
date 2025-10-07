02a81a99afcc5fb7915173600cad51933db76bcf
Maximo <Maximokugler9@gmail.com>
2025-10-07 13:02:43 -0300

    Adjustment

--- Backend/src/Index.js
+++ Backend/src/Index.js
@@ -25,7 +25,7 @@ app.use(logger("dev"));
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://dialogue-chatmk.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
